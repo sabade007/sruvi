a1d1deb220a00f01a94f8a9f45d7b57eeef4652f
sabade007 <homesabade@gmail.com>
2024-06-04 17:38:04 +0530

    v0.003

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline font-bungee">Hello World</h1>
+      <h1 className="text-3xl font-bold underline font-bungee text-primary">
+        Hello World
+      </h1>
     </div>
   );
 }
