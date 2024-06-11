7942aebad7618b2dc3f37c110c89669921378073
sabade007 <homesabade@gmail.com>
2024-06-11 09:22:36 +0530

    latest10-06-2027

---
+++ src/animations/Animatediv.js
@@ -0,0 +1,24 @@
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
+
+function Animatediv({ children }) {
+  const ref = useRef();
+
+  return (
+    <motion.div
+      ref={ref}
+      initial="hidden"
+      animate={inView ? "visible" : "hidden"}
+      variants={{
+        visible: { opacity: 1, scale: 1 },
+        hidden: { opacity: 0, scale: 0 },
+      }}
+      transition={{ duration: 0.3 }}
+      viewport={{ once: true }} // Add this line
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default Animatediv;
